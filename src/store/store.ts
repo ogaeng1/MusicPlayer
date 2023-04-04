548dd8ebdd7982a94e43b5c5307cf844f6d2c94f
OH <rudals2148@naver.com>
2023-04-04 22:54:53 +0900

    [new] 스토어 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,32 @@
 import { atom } from "recoil";
+import { musicData } from "../data/music";
 
 export const PlayButtonState = atom({
     key: "PlayButtonState",
     default: false
 })
 
-// export const PlayingSong = atom({
-//     key:
+export const CurrentTimeState = atom({
+    key: "CurrentTimeState",
+    default: 0
+})
+
+export const DurationState = atom({
+    key: "DurationState",
+    default: 0
+})
+
+export const CurrentIndexState = atom({
+    key: "CurrentIndexState",
+    default: 0
+})
+
+// export const ProgressState = atom({
+//     key: "ProgressState",
+//     default: 0
+// })
+
+// export const PlayListState = atom({
+//     key: "PlayListState",
+//     default: [...musicData]
 // })
